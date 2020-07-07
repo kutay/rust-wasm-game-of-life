644c1b97864a0f7c0fd5232bcb2c647bff791bbf
Aykut ACIKEL <aykut.acikel@gmail.com>
2020-07-07 12:12:45 +0200

    Fix Netlify script

--- netlify.sh
+++ netlify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl https://sh.rustup.rs -sSf | sh -s - --default-toolchain nightly -y
-
+source ~/.cargo/env
 
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 wasm-pack build
